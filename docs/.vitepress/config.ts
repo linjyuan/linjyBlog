34a4ada2a09996e0c6cd5a92632a56886ab5bce4
linjy <lin.junyuan2@iwhalecloud.com>
2025-02-19 22:31:05 +0800

    init: 仓库初始化

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       }
     ],
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/linjyuan/linjyBlog.git' }
     ]
   },
   outDir: '../../dist',
